e4d36103e79047fdea4b88d4bed1e3bdb7b6b4f8
Alison Yeung <ayeungv@ucenfotec.ac.cr>
2026-08-06 20:11:58 -0600

    feat(PP-69): add EnlacePerfilDTO interface - task 8.1

---
+++ src/app/pages/perfil-publico/models/enlace-perfil.model.ts
@@ -0,0 +1,9 @@
+export interface EnlacePerfilDTO {
+  urlCanonica: string;
+  codigoIncrustar: string;
+  qrBase64: string;
+  ogTitulo: string;
+  ogDescripcion: string;
+  ogImagen: string;
+  ogUrl: string;
+}
