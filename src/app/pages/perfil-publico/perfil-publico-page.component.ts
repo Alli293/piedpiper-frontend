47fd7bc5baa706b707b0b161c88a0d275226022e
Alison Yeung <ayeungv@ucenfotec.ac.cr>
2026-08-06 21:50:55 -0600

    refactor(enlace-comparticion): move sharing tools to header button + modal dialog

--- src/app/pages/perfil-publico/perfil-publico-page.component.ts
+++ src/app/pages/perfil-publico/perfil-publico-page.component.ts
@@ -72,6 +72,7 @@ export class PerfilPublicoPageComponent {
   protected insignias = signal<InsigniaEmpresa[]>([]);
   protected evolucion = signal<EvolucionHuellaPublica | null>(null);
   protected mensajeError = signal<string>('');
+  protected mostrarCompartir = signal<boolean>(false);
 
   protected readonly maxTco2e = computed(() => {
     const ev = this.evolucion();
@@ -187,6 +188,14 @@ export class PerfilPublicoPageComponent {
 
   protected reintentar(): void {
     this.cargar();
+  }
+
+  protected toggleCompartir(): void {
+    this.mostrarCompartir.update((v) => !v);
+  }
+
+  protected cerrarCompartir(): void {
+    this.mostrarCompartir.set(false);
   }
 
   protected readonly nivelConfig = computed(() => {
